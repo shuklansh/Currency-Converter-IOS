96283e438981d79cd574354c38903d98af3c7b0e
anshArtium <ansh@artiumacademy.com>
2023-11-17 19:29:32 +0530

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by www.abcom.in on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
